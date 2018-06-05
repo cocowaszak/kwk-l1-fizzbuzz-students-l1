74fc622f117e94232713b40331f9afda3240c638
cocowaszak <colettewaszak@gmail.com>
2018-06-05 19:08:49 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3 
-  "Fizz" # Go Fizz
+  "fizzbuzz(3)" # Go Fizz
 end 
 end 
 
 fizzbuzz(3) # => you should see a return of "Fizz"
 fizzbuzz(5) # => you should see a return of nil
+fizzbuzz() # => you should get an ArgumentError
