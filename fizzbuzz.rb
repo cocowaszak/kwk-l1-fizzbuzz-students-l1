a21e9b695f1bdb4310709712adf2ccb880e3f34c
cocowaszak <colettewaszak@gmail.com>
2018-06-05 19:07:50 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,8 @@
 def fizzbuzz(int)
   if int % 3 == 0 # if the number int is divisible by 3 
-  "Fizz(3)" # Go Fizz
+  "Fizz" # Go Fizz
 end 
 end 
+
+fizzbuzz(3) # => you should see a return of "Fizz"
+fizzbuzz(5) # => you should see a return of nil
